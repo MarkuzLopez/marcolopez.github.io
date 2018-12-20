dd1014060ef6b25b3b09d0a9460e8a7f96ced3bb
Mark02Lopez <mamorales@todoensubastas.com.mx>
2018-12-20 14:24:59 -0600

    primer despliegue a github react

---
+++ precache-manifest.8466affe7e52f1c6ae711b7335da23e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "404e8b85015524db8707",
+    "url": "/static/js/main.404e8b85.chunk.js"
+  },
+  {
+    "revision": "d2eff4d94d5b9d29e4ec",
+    "url": "/static/js/1.d2eff4d9.chunk.js"
+  },
+  {
+    "revision": "404e8b85015524db8707",
+    "url": "/static/css/main.5537cfa2.chunk.css"
+  },
+  {
+    "revision": "ac9c1333ab187fc6970ce94d2eaabcf8",
+    "url": "/index.html"
+  }
+];
